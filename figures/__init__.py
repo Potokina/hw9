d9a302bc13060396c26fa6ca9032bfed54508eb4
User <polina@potok.in>
2023-02-07 17:09:38 +0300

    hello

---
+++ figures/__init__.py
@@ -0,0 +1,12 @@
+from .circle.code import circle_area, circle_perimeter
+from .square.code import square_area, square_perimeter
+from .triangle.code import triangle_perimrter,triangle_area
+
+__all__ = ("circle_area",
+           "circle_perimeter",
+           "square_area",
+           "square_perimeter",
+           "triangle_perimrter",
+           "triangle_area")
+
+#hello
